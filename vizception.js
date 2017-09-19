cecf1b0fa1fbf59f296d893483d02a6e2146ac29
Chris DeMartini <demartsc@Chriss-MacBook-Pro.local>
2017-09-18 23:39:18 -0700

    added window.top to js

--- vizception.js
+++ vizception.js
@@ -9,7 +9,7 @@ TABLEAU_NULL = '%null%';
 //document.domain = 'brilliant-data.net';
 
 getTableau = function() {
-      return parent.parent.tableau;
+      return window.top.tableau || parent.parent.tableau;
 };
 
 getCurrentViz = function() {
